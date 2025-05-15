022922779906ebc769d166451a58ca3c253cdc00
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-15 15:41:59 +0700

    Step 9

--- DoubleLinkekList.cpp
+++ DoubleLinkekList.cpp
@@ -61,5 +61,9 @@ class DoubleLinkedLis{
             cout << "\nDuplicate roll numbers not allowed" << endl;
             return;
         }
+
+        //step 9 : Insert between current and current->next
+        newNode->next = current->next;  // Step 9a : newNode.next = current.next
+        newNode->prev = current;        // step 9b : newNode.prev = current
     }
 };
