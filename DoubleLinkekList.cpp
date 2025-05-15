3909aeaab7759ec63b9f459609dfde34b16f73b6
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-15 16:02:43 +0700

    Bila NIM tidak ditemukan

--- DoubleLinkekList.cpp
+++ DoubleLinkekList.cpp
@@ -80,6 +80,18 @@ class DoubleLinkedLis{
 
         cout << "\nMasukkan NIM yang akan didelete : ";
         int rollNo;
-        cin >> rollNo
+        cin >> rollNo;
+
+        Node *current = START;
+
+        //Step 1 : Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo){
+            current = current->next;
+        }
+
+        if (current == NULL){
+            cout << "Record not found" << endl;
+            return;
+        }
     }
 };
