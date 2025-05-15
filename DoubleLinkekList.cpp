fc350b92918aafcf5661e9f7950918b8a6525eb9
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-15 15:38:13 +0700

    Melanjutkan step 8

--- DoubleLinkekList.cpp
+++ DoubleLinkekList.cpp
@@ -38,7 +38,7 @@ class DoubleLinkedLis{
             newNode->next = START;
 
             //step 5 : START.prev = newNode (if START exists)
-            if (START != NULL)
+            if (START != NULL){
                 START->prev = newNode;
 
                 //step 6 : newNode.prev = NULL
@@ -47,11 +47,19 @@ class DoubleLinkedLis{
                 //step 7 : START = newNode
                 START = newNode;
                 return;
+            }
         }
 
         //insert in between node
         //step 8 : locate psisition for insertion
         Node *current = START;
-        while(current->next != NULL && current->next->noMhs <nim)
+        while(current->next != NULL && current->next->noMhs <nim){
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs){
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
     }
 };
